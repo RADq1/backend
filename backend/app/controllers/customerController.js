345c61c77af0cacad88d2af1c666c1c16147d8d6
RADq1 <1998radq@gmail.com>
2022-08-10 19:02:47 +0200

    10.08.2022
    delete, edit rest api

--- backend/app/controllers/customerController.js
+++ backend/app/controllers/customerController.js
@@ -28,8 +28,28 @@ controller.createTables = async (req,res) => {
             email: '1998radq@gmail.com',
             address: 'Fordońska 431a/3',
             phone: '661614088',
-            roleId: 1
+            roleId: 1,
         });
+        Role.create({
+            role:'Project Manager'
+        });
+        Role.create({
+            role:'Programmer'
+        })
+        Employee.create({
+          name: 'Ania',
+          email: '1998ania@gmail.com',
+          address: 'Fordońska 431a/3',
+          phone: '883777158',
+          roleId: 2,
+        })
+        Employee.create({
+          name: 'Damian',
+          email: '2000damian@gmail.com',
+          address: 'Strzelecka x/y',
+          phone: 'xyz123456',
+          roleId: 3,
+        })
         const data = Employee.findAll();
         return data;
     })
@@ -38,7 +58,20 @@ controller.createTables = async (req,res) => {
     });
     res.json({success: true, data: response });
 }
-
+controller.get = async (req,res) => {
+  const { id } = req.params;
+  const data = await Employee.findAll({
+      where: { id: id },
+      include: [ Role ]
+  })
+  .then(function(data){
+    return data;
+  })
+  .catch(error =>{
+    return error;
+  })
+  res.json({ success: true, data: data });
+}
 controller.list = async (req, res) => {
     //funkcja do pobierania elementów z tabeli Employee
     const data = await Employee.findAll({
@@ -54,8 +87,7 @@ controller.list = async (req, res) => {
     res.json({success : true, data : data});
 
   }
-
-  controller.create = async (req,res) => {
+controller.create = async (req,res) => {
     // data
     const { name, email, address, phone, role } = req.body;
     // create
@@ -80,4 +112,45 @@ controller.list = async (req, res) => {
       data: data
     });
   }
+  controller.update = async (req,res) => {
+    // parameter get id
+    const { id } = req.params;
+    // parameter POST
+    const {name, email, address, phone, role } = req.body;
+    console.log(name, email, address, phone, role);
+    // Update data
+    const data = await Employee.update({
+      name:name,
+      email:email,
+      address:address,
+      phone:phone,
+      roleId:role
+    },
+    {
+      where: { id: id}
+    })
+    .then(function(data){
+      return data;
+    })
+    .catch(error => {
+      return error;
+    })
+    // console.log(data.name);
+    res.status(200).json({
+      success: true,
+      message:"Pracownik updated",
+      data: data
+    });
+    // res.json({success:true, data:data, message:"Updated successful"});
+  }
+  controller.delete = async (req, res) => {
+    // parameter post
+    const { id } = req.body;
+    // delete sequelize
+    const del = await Employee.destroy({
+      where: { id: id}
+    })
+    res.json({success:true,deleted:del,message:"Deleted successful"});
+  }
+
 module.exports = controller;
--- backend/app/routes/employeeRoute.js
+++ backend/app/routes/employeeRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const employeeController = require('../controllers/customerController')
+const employeeController = require('../controllers/customerController');
+const Employee = require('../model/employee');
 
 // router.get('/waka', (req,res) => {
 //     res.json({status: 'Employeed Saved'});
@@ -9,8 +10,9 @@ const employeeController = require('../controllers/customerController')
 // router.get('/test', employeeController.test);
 //tworzenie tabeli do bazy danych -- baze danych stworzylem sam, phpmyadmin
 router.get('/createTable', employeeController.createTables);
-
 router.get('/list', employeeController.list);
 router.post('/create',employeeController.create);
-
+router.get('/get/:id', employeeController.get);
+router.post('/update/:id', employeeController.update);
+router.post('/delete', employeeController.delete);
 module.exports = router;
